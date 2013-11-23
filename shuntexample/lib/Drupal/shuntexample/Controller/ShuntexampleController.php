cd64938ad23461509460cc1607bad3489f120be7
TravisCarden <travis.carden@gmail.com>
2013-11-23 15:52:09 -0600

    By TravisCarden: Added a working example page to Shunt Example module.

---
+++ shuntexample/lib/Drupal/shuntexample/Controller/ShuntexampleController.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\shunt\Controller\ShuntexampleController.
+ */
+
+namespace Drupal\shuntexample\Controller;
+
+/**
+ * Controller class for the Shunt Example module.
+ */
+class ShuntexampleController {
+
+  /**
+   * Route callable method.
+   *
+   * @return array
+   *   A theme array.
+   *
+   * @see shuntexample-hello.html.twig
+   * @see shuntexample-fail.html.twig
+   */
+  public function hello() {
+    // Fail cheap if the "shuntexample" shunt is enabled.
+    if (module_exists('shunt') && shunt_is_enabled('shuntexample')) {
+      return array('#theme' => 'shuntexample_fail');
+    }
+
+    // Expensive processing can be done down here.
+    return array('#theme' => 'shuntexample_hello');
+  }
+}
