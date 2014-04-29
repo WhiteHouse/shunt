4cb495294fe5cf9f45c7e8d1d1c4a0518c7f6ed4
TravisCarden <travis.carden@gmail.com>
2014-04-28 22:40:25 -0500

    By TravisCarden: Added some test coverage for the config form.

---
+++ src/Tests/ShuntUiTest.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\shunt\Tests\ShuntUiTest.
+ */
+
+namespace Drupal\shunt\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests the Shunt UI.
+ */
+class ShuntUiTest extends WebTestBase {
+
+  const CONFIG_FORM_PATH = 'admin/config/development/shunts';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = array('shunt', 'shuntexample');
+
+  /**
+   * @var \Drupal\user\Entity\User
+   *   A user object with permission to administer shunts.
+   */
+  protected $privilegedUser;
+
+  /**
+   * @var \Drupal\shunt\ShuntHandler
+   *   The shunt handler.
+   */
+  protected $shuntHandler;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'Shunt UI',
+      'description' => 'Tests the Shunt web UI.',
+      'group' => 'Shunt',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->shuntHandler = \Drupal::service('shunt.handler');
+
+    $permissions = array('administer shunts');
+    $this->privilegedUser = $this->drupalCreateUser($permissions);
+    $this->drupalLogin($this->privilegedUser);
+  }
+
+  /**
+   * Asserts that a shunt has a given status.
+   *
+   * @param string $name
+   *   The shunt name.
+   * @param bool $status
+   *   The shunt status--TRUE for enabled or FALSE for disabled.
+   * @param string $message
+   *   (optional) A message to display with the assertion. Do not translate
+   *   messages: use format_string() to embed variables in the message text, not
+   *   t(). If left blank, a default message will be displayed.
+   */
+  protected function assertShuntStatus($name, $status, $message = '') {
+    $actual = $this->shuntHandler->isEnabled($name);
+    $expected = (bool) $status;
+    $this->assertIdentical($actual, $expected, $message);
+  }
+
+  /**
+   * Tests config form access.
+   */
+  public function testConfigFormAccess() {
+    $this->drupalGet($this::CONFIG_FORM_PATH);
+    $this->assertResponse(200, 'Granted access to config form to privileged user.');
+
+    $this->drupalLogout();
+    $this->drupalGet($this::CONFIG_FORM_PATH);
+    $this->assertResponse(403, 'Denied access to config form non-privileged user.');
+  }
+
+  /**
+   * Tests making shunt status changes through the config form.
+   */
+  public function testConfigFormStatusChanges() {
+    $this->assertShuntStatus('shunt', FALSE, 'Shunt "shunt" was disabled by default.');
+    $this->assertShuntStatus('shuntexample', FALSE, 'Shunt "shuntexample" was disabled by default.');
+
+    $edit_enable_shunt['shunts[shunt]'] = 'shunt';
+    $edit_enable_shunt['shunts[shuntexample]'] = FALSE;
+    $this->drupalPostForm($this::CONFIG_FORM_PATH, $edit_enable_shunt, t('Save configuration'));
+    $this->assertShuntStatus('shunt', TRUE, 'Shunt "shunt" was enabled.');
+    $this->assertText(t('Shunt "shunt" has been enabled.'), 'Displayed message for enabled shunt');
+    $this->assertShuntStatus('shuntexample', FALSE, 'Shunt "shuntexample" was unchanged.');
+    $this->assertNoText(t('Shunt "shuntexample" has been enabled.'), 'Did not display message for unchanged shunt');
+
+    $edit_enable_shuntexample['shunts[shunt]'] = FALSE;
+    $edit_enable_shuntexample['shunts[shuntexample]'] = 'shuntexample';
+    $this->drupalPostForm($this::CONFIG_FORM_PATH, $edit_enable_shuntexample, t('Save configuration'));
+    $this->assertShuntStatus('shunt', FALSE, 'Shunt "shunt" was disabled.');
+    $this->assertText(t('Shunt "shunt" has been disabled.'), 'Displayed message for disabled shunt');
+    $this->assertShuntStatus('shuntexample', TRUE, 'Shunt "shuntexample" was enabled.');
+    $this->assertText(t('Shunt "shuntexample" has been enabled.'), 'Displayed message for enabled shunt');
+
+    $edit_disable_both['shunts[shunt]'] = FALSE;
+    $edit_disable_both['shunts[shuntexample]'] = FALSE;
+    $this->drupalPostForm($this::CONFIG_FORM_PATH, $edit_disable_both, t('Save configuration'));
+    $this->assertShuntStatus('shunt', FALSE, 'Shunt "shunt" was unchanged.');
+    $this->assertNoText(t('Shunt "shunt" has been disabled.'), 'Did not display message for unchanged shunt');
+    $this->assertShuntStatus('shuntexample', FALSE, 'Shunt "shuntexample" was disabled.');
+    $this->assertText(t('Shunt "shuntexample" has been disabled.'), 'Displayed message for disabled shunt');
+  }
+
+}
