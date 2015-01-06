5ea701c8dadd6114be957193175f1d4ff57fb0bd
TravisCarden <travis.carden@gmail.com>
2015-01-05 23:16:24 -0600

    By TravisCarden: Refactored run-tests.sh to be independent of script directory.

--- run-tests.sh
+++ run-tests.sh
@@ -1,31 +1,40 @@
 #!/usr/bin/env sh
 
 # @file
-# Runs automated tests for Shunt module.
+# Runs automated tests for the Shunt module.
 #
-# This script assumes that the module is installed in
-# [drupal-docroot]/modules/contrib/shunt.
-#
-# Usage ./run-tests [webserver-user] (defaults to "www-data") [URI] (defaults to
-#   "http://d8.dev/"
-#   e.g., ./run-tests
-#   or ./run-tests apache http://example.com/
+# Usage ./run-tests.sh [$WEBSERVER_USER] (defaults to "www-data") [$URI]
+#   (defaults to "http://d8.dev/")
+#   e.g., ./run-tests.sh
+#   or ./run-tests.sh apache http://example.com/
 #
 # Note: You will be prompted for your sudo password to run as the webserver
 # user.
 
+# Get the directory the current script is in.
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Abort if Drush isn't available.
+if ! command -v drush >/dev/null 2>&1; then
+  echo >&2 "This script requires Drush, which is not currently installed. Visit http://drush.org/ to learn how to install it."
+  exit 1
+fi
+
+# Get the Drupal root directory. Abort if none is found.
+DRUPAL_ROOT=$(drush dd 2>&1)
+if [ $? -eq 1 ]; then
+  echo >&2 "The Drupal root directory could not be found. Please run from within a functioning Drupal site."
+  exit 1
+fi
+
+# Run PHPUnit tests.
+echo "Running PHPUnit tests..."
+${DRUPAL_ROOT}/core/vendor/bin/phpunit --configuration="$DRUPAL_ROOT/core" "$SCRIPT_DIR/tests/src"
+echo
+
+# Run Simpletest tests.
+echo "Running Simpletest tests..."
 WEBSERVER_USER=${1:-"www-data"}
 URI=${2:-"http://d8.dev/"}
-
-run_phpunit_tests() {
-  cd ../../../core/
-  ./vendor/bin/phpunit \
-    ../modules/contrib/shunt/tests/src/
-}
-
-run_simpletest_tests() {
-  sudo -u ${WEBSERVER_USER} php scripts/run-tests.sh --url ${URI} shunt
-}
-
-run_phpunit_tests
-run_simpletest_tests
+sudo -u ${WEBSERVER_USER} php ${DRUPAL_ROOT}/core/scripts/run-tests.sh --url ${URI} shunt
+echo
